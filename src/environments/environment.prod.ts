fa9984d7f9999ef02c233c4e35d99c1896e0aee1
M3rzh1n <96054590+M3rzh1n@users.noreply.github.com>
2023-03-20 08:34:05 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
     domain: 'dev-isoft.us.auth0.com',
     clientId: 'lERzBtf7cxB37irvf5ON8ZPjyXXnYYyR',
     authorizationParams: {
-      redirect_uri: 'https://github.com/M3rzh1n/Angular_Typescript_App1/callback',
+      redirect_uri: 'https://m3rzh1n.github.io/Angular_Typescript_App1/callback',
       connection: "sms",
     },
     errorPath: '/callback',
